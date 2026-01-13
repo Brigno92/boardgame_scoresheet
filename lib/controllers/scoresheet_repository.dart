9a890030370966034a64c28954aff2f9d0f43477
Daniele Brignole <daniele.brignole@bss.group>
2026-01-13 10:28:54 +0100

    added insert sheet and sheet dto

--- lib/controllers/scoresheet_repository.dart
+++ lib/controllers/scoresheet_repository.dart
@@ -1,4 +1,5 @@
 import 'package:boardgame_scoresheet/controllers/app_database.dart';
+import 'package:boardgame_scoresheet/dtos/scoresheet_dto.dart';
 import 'package:boardgame_scoresheet/entities/boardgame_sheet.dart';
 import 'package:boardgame_scoresheet/entities/score_row.dart';
 import 'package:sqflite/sqflite.dart';
@@ -13,6 +14,16 @@ class ScoresheetRepository {
       item,
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
+  }
+
+  Future<int> createSheet(ScoresheetDto dto) async {
+    final dbFuture = await _appDatabase.database;
+    var sheetId = await dbFuture.insert('bg_sheet', dto.toMap());
+    for (var row in dto.scoreRows) {
+      row.sheetId = sheetId;
+      await dbFuture.insert('score_row', row.toMap());
+    }
+    return sheetId;
   }
 
   Future<int> deleteSheet(int id) async {
@@ -36,16 +47,11 @@ class ScoresheetRepository {
     var returnSheet = BoardgameSheet(
       id: id,
       name: sheet.first['name'] as String,
-      maxPlayers: sheet.first['maxPlayers'] as int
+      maxPlayers: sheet.first['maxPlayers'] as int,
     );
     returnSheet.scoreRows = rows.map((rowMap) {
       var row = rowMap['score_row']!;
-      return ScoreRow(
-        row.name,
-        row.positiveScore,
-        row.id,
-        row.sheetId
-      );
+      return ScoreRow(row.name, row.positiveScore, row.id, row.sheetId);
     }).toList();
 
     return returnSheet;
